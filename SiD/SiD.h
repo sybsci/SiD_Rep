f241c73c6b06e7ffc668c332af3b7a575ac48447
sybsci <stein13@bk.ru>
2014-08-12 22:14:15 +0700

    SiD 1

---
+++ SiD/SiD.h
@@ -0,0 +1,32 @@
+
+// SiD.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CSiDApp:
+// See SiD.cpp for the implementation of this class
+//
+
+class CSiDApp : public CWinApp
+{
+public:
+	CSiDApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSiDApp theApp;
---
+++ SiD/SiDDlg.h
@@ -0,0 +1,32 @@
+
+// SiDDlg.h : header file
+//
+
+#pragma once
+
+
+// CSiDDlg dialog
+class CSiDDlg : public CDialogEx
+{
+// Construction
+public:
+	CSiDDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_SID_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
